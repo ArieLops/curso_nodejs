b11c25cfe859ff9022238040570af8b8edf09464
ariel <arielalmeidalopes@gmail.com>
2019-12-17 00:56:05 -0300

    Aula 01

---
+++ app.js
@@ -0,0 +1,11 @@
+//Aula 01
+var  mostrarSite = false;
+const site = "www.google.com.br";
+
+console.log("Hello World");
+
+//Se mostrar site
+if(mostrarSite){
+    //printa site
+    console.log(site);
+}
