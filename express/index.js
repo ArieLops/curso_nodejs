ba66e562567d0998f203aeb8432d8c1bd41340a1
ariel <arielalmeidalopes@gmail.com>
2019-12-22 22:57:17 -0300

    Rotas

--- express/index.js
+++ express/index.js
@@ -4,6 +4,20 @@ const express = require("express");
 //Iniciando o express e passando para APP
 const app = express();
 
+//Rotas
+app.get("/", function(requisicao, resposta){
+    resposta.send("Bem vindo!");
+});
+
+app.get("/blog", function(requisicao, resposta){
+    resposta.send("Blog");
+});
+
+app.get("/canal/youtube", function(requisicao, resposta){
+    resposta.send("Canal Youtube");
+});
+
+//Criação do servidor
 app.listen(4000, function(erro){
     if(erro){
         console.log("Ocorreu um erro");
